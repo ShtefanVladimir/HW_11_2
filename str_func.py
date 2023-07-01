1664c917c4388b5de64a7f3f3e861f63fcfa46a0
Vladimir Shtefan <vladimir.shtefan@skyeng.ru>
2023-07-01 19:51:54 +0700

    added func get_upper_str

--- str_func.py
+++ str_func.py
@@ -1,5 +1,23 @@
 def get_title_str(str_):
     """
+
+    :param str_:
+    :return:
+    """
+    return str_.title()
+
+
+def get_upper_str(str_):
+    """
+
+    :param str_:
+    :return:
+    """
+    return str_.upper()
+
+
+def get_title_str_1(str_):
+    """
     Docstring
     """
     return str_.title()
